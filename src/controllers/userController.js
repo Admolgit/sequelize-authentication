3b6bfe914c8c82ad42e2a8ce882145acd8040a0b
Admolgit <zealous890@gmail.com>
2023-03-16 18:21:28 +0100

    User Updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,26 +1,28 @@
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
 const db = require("../models");
 const { hashPassword } = require("../utils");
+
+dotenv.config();
 
 // Create main model
 const User = db.users;
 
 // Create product
 module.exports.createUser = async (req, res) => {
-  let { fullName, email, password } = req.body;
+  const { fullName, email } = req.body;
+  let { password } = req.body;
   try {
     if (!fullName || !email || !password)
       throw new error("All fields must be filled");
 
     const hashedPassword = await hashPassword(password);
 
-    // const password = passwordHashed
-
     const userExist = await User.findOne({
       where: {
         email: email,
-       }
+      },
     });
 
     if (userExist)
@@ -28,7 +30,7 @@ module.exports.createUser = async (req, res) => {
         message: "Email has already been registered",
       });
 
-    password = hashedPassword
+    password = hashedPassword;
 
     const user = await User.create({
       fullName,
@@ -53,8 +55,11 @@ module.exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) throw new error("All fields must be filled");
+
     const userExist = await User.findOne({
-      email: email,
+      where: {
+        email: email,
+      },
     });
 
     if (!userExist) {
@@ -62,11 +67,90 @@ module.exports.login = async (req, res) => {
         message: "User does not exist",
       });
     } else {
+      const hashedPasword = userExist.dataValues.password;
+      if (bcrypt.compareSync(password, hashedPasword)) {
+        const token = jwt.sign({ data: userExist }, process.env.JWT_SECRET);
+        res.status(200).json({
+          message: "User logged in successfully",
+          token: token,
+          data: userExist,
+        });
+      }
     }
   } catch (error) {
     res.status(500).json({
       message: "Something went wrong",
       error: error.message,
     });
+  }
+};
+
+module.exports.getUsers = async (req, res) => {
+  const users = await User.findAll();
+
+  if (!users) {
+    return res.status(404).json({
+      message: "No users found in the database",
+    });
+  } else {
+    res.status(200).json({
+      message: "Users fetched successfully",
+      data: users,
+    });
+  }
+};
+
+module.exports.getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findOne({
+      where: {
+        id: id,
+      },
+    });
+  
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found in the database",
+      });
+    } else {
+      res.status(200).json({
+        message: "User fetched successfully",
+        data: user,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error: error.message
+    })
+  }
+};
+
+module.exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.destroy({
+      where: {
+        id: id,
+      },
+    });
+  
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found in the database",
+      });
+    } else {
+      res.status(200).json({
+        message: "User deleted successfully",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error: error.message
+    })
   }
 };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { createUser } = require('../controllers/userController');
+const { createUser, login, getUsers, getUserById, deleteUser } = require('../controllers/userController');
 
 const userRouter = express.Router();
 
 userRouter.post('/create-user', createUser);
+userRouter.post('/login-user', login);
+userRouter.get('/users', getUsers);
+userRouter.get('/user/:id', getUserById);
+userRouter.delete('/user/:id', deleteUser);
 
 module.exports = userRouter;
